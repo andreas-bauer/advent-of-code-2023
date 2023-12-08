3d8459c8ffe781a3f366eaa933c177a7376e75d4
Andreas Bauer <dev@andreasbauer.org>
2023-12-08 22:35:19 +0100

    Add day01 part 2

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -3,27 +3,44 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    let mut sum = 0;
     let lines_result = read_lines("input/day01.txt");
     let lines = match lines_result {
         Ok(l) => l,
         Err(error) => panic!("Unable to open the file: {:?}", error),
     };
 
+    let mut sum1: u32 = 0;
+    let mut sum2: u32 = 0;
+
     for line in lines.into_iter().flatten() {
-        let line_digits: Vec<_> = line.chars().filter(|c| c.is_ascii_digit()).collect();
+        // Part 1
+        let first_c = line.chars().find(|c| c.is_ascii_digit()).unwrap();
+        let last_c = line.chars().rfind(|c| c.is_ascii_digit()).unwrap();
+        let result: u32 = format!("{}{}", first_c, last_c).parse().unwrap();
+        sum1 += result;
 
-        if line_digits.is_empty() {
-            continue;
-        }
+        // Part 2
+        let fix = line
+            .replace("one", "o1e")
+            .replace("two", "t2o")
+            .replace("three", "th3ee")
+            .replace("four", "f4ur")
+            .replace("five", "fi5e")
+            .replace("six", "s6x")
+            .replace("seven", "se7en")
+            .replace("eight", "ei8ht")
+            .replace("nine", "n9ne")
+            .replace("zero", "z0ro");
 
-        let first = line_digits.first().unwrap();
-        let last = line_digits.last().unwrap();
-        let result: u32 = format!("{}{}", first, last).parse().unwrap();
+        let first_c = fix.chars().find(|c| c.is_ascii_digit()).unwrap();
+        let last_c = fix.chars().rfind(|c| c.is_ascii_digit()).unwrap();
+        let result: u32 = format!("{}{}", first_c, last_c).parse().unwrap();
 
-        sum += result;
+        sum2 += result;
     }
-    println!("Sum: {}", sum);
+
+    println!("Sum part 1: {}", sum1);
+    println!("Sum part 2: {}", sum2);
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
