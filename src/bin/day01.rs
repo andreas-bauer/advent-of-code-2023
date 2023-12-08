9a301083b89ca8b214c0427a8d42773cee08df06
Andreas Bauer <dev@andreasbauer.org>
2023-12-08 08:39:22 +0100

    Reafactor day01 based on Clippy suggestions

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -10,32 +10,26 @@ fn main() {
         Err(error) => panic!("Unable to open the file: {:?}", error),
     };
 
-    for line_result in lines {
-        if let Ok(line) = line_result {
-            let line_digits: Vec<_> = line.chars()
-                .filter(|c| c.is_digit(10))
-                .collect();
+    for line in lines.into_iter().flatten() {
+        let line_digits: Vec<_> = line.chars().filter(|c| c.is_ascii_digit()).collect();
 
-            if line_digits.is_empty() {
-                continue;
-            }
-
-            let first = line_digits.first().unwrap();
-            let last = line_digits.last().unwrap();
-            let result: u32 = format!("{}{}", first, last).parse().unwrap();
-
-            sum += result;
-
-            // println!("{}", line);
-            // println!("line_digits: {}", line_digits.len());
-            // println!("{} + {} = {}", first, last, result)
+        if line_digits.is_empty() {
+            continue;
         }
+
+        let first = line_digits.first().unwrap();
+        let last = line_digits.last().unwrap();
+        let result: u32 = format!("{}{}", first, last).parse().unwrap();
+
+        sum += result;
     }
     println!("Sum: {}", sum);
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
