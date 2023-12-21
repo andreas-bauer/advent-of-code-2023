19a7727978c7f772bc590f4fd3cd619ff9a81a96
Andreas Bauer <dev@andreasbauer.org>
2023-12-21 22:08:27 +0100

    Add day05 part 2

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -1,7 +1,4 @@
 use std::fs;
-use std::cmp;
-
-const IDX_SEED: usize = 0;
 
 #[derive(Debug)]
 struct Converter {
@@ -22,9 +19,12 @@ fn main() {
         Err(error) => panic!("Unable to open the file: {:?}", error),
     };
 
-    let input_parts:Vec<&str> = file.split("\n\n").collect::<Vec<_>>();
+    let input_parts: Vec<&str> = file.split("\n\n").collect::<Vec<_>>();
 
-    let seeds: Vec<i64> = input_parts.get(IDX_SEED).unwrap()
+    println!("=== Part 1 ===");
+    let seeds: Vec<i64> = input_parts
+        .first()
+        .unwrap()
         .split_whitespace()
         .skip(1)
         .map(|c| c.parse::<i64>().unwrap())
@@ -32,38 +32,71 @@ fn main() {
 
     let mut pipeline: Vec<PipeStep> = Vec::with_capacity(7);
     for idx in 1usize..=7usize {
-        pipeline.push(
-            PipeStep {
-                _id: idx,
-                converters: parts_to_converters(input_parts.get(idx).unwrap())
+        pipeline.push(PipeStep {
+            _id: idx,
+            converters: parts_to_converters(input_parts.get(idx).unwrap()),
+        })
+    }
+
+    let lowest_p1 = seeds
+        .iter()
+        .map(|seed| process_pipeline(*seed, &pipeline))
+        .min()
+        .unwrap();
+
+    println!("Lowest location = {lowest_p1}");
+
+    println!("=== Part 2 ===");
+
+    let seeds_p2: Vec<_> = seeds
+        .chunks(2)
+        .map(|chunk| {
+            let start = *chunk.first().unwrap();
+            let count = *chunk.get(1).unwrap();
+            std::ops::Range {
+                start,
+                end: start + count,
             }
-        )
+        })
+        .collect();
+
+    let lowest_p2 = seeds_p2
+        .iter()
+        .map(|seed_range| {
+            seed_range
+                .clone()
+                .map(|seed| process_pipeline(seed, &pipeline))
+                .min()
+                .unwrap()
+        })
+        .min()
+        .unwrap();
+
+    println!("Lowest location = {lowest_p2}");
+}
+
+fn process_pipeline(seed: i64, pipeline: &Vec<PipeStep>) -> i64 {
+    let mut loc: i64 = seed;
+    for step in pipeline {
+        loc = apply_converts(loc, &step.converters);
     }
 
-    let mut lowest: i64 = i64::MAX;
-    for seed in seeds {
-        let mut loc: i64 = seed;
-        for step in &pipeline {
-            loc = apply_converts(loc, &step.converters);
-        }
-        lowest = cmp::min(lowest, loc);
-        println!("Input seed = {} output pos = {}",seed,loc);
-    }
-
-    println!("Lowest location = {lowest}");
+    loc
 }
 
 fn parts_to_converters(input: &&str) -> Vec<Converter> {
-    input.lines()
+    input
+        .lines()
         .skip(1)
         .map(|line| {
-            let num: Vec<_> = line.split_whitespace()
-            .map(|c| c.parse::<i64>().unwrap())
-            .collect();
+            let num: Vec<_> = line
+                .split_whitespace()
+                .map(|c| c.parse::<i64>().unwrap())
+                .collect();
 
             Converter {
                 range_start: num[1],
-                range_end: num[1] + num[2] -1,
+                range_end: num[1] + num[2] - 1,
                 offset: num[0] - num[1],
             }
         })
