7102e0e859e5f1a19fd5c4459877c458c256e98d
Andreas Bauer <dev@andreasbauer.org>
2023-12-21 23:37:17 +0100

    Add day06 part 2

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,49 +1,75 @@
 struct Game {
-    time: u32,
-    dist: u32,
+    time: u64,
+    dist: u64,
 }
 
 fn main() {
-    let g1 = Game { time: 47, dist: 207 };
-    let g2 = Game { time: 84, dist: 1394 };
-    let g3 = Game { time: 74, dist: 1209 };
-    let g4: Game = Game { time: 67, dist: 1014 };
+    println!("=== Part 1 ===");
+    process_part1();
 
-    let g1_win = solutions_for_game(g1);
-    let g2_win = solutions_for_game(g2);
-    let g3_win = solutions_for_game(g3);
-    let g4_win = solutions_for_game(g4);
+    println!("=== Part 2 ===");
+    process_part2();
+}
 
-    println!("G1 wins: {}", g1_win.len());
-    println!("G2 wins: {}", g2_win.len());
-    println!("G3 wins: {}", g3_win.len());
-    println!("G4 wins: {}", g4_win.len());
+fn process_part1() {
+    let g1 = Game {
+        time: 47,
+        dist: 207,
+    };
+    let g2 = Game {
+        time: 84,
+        dist: 1394,
+    };
+    let g3 = Game {
+        time: 74,
+        dist: 1209,
+    };
+    let g4 = Game {
+        time: 67,
+        dist: 1014,
+    };
 
-    let sum = g1_win.len() * g2_win.len() * g3_win.len() * g4_win.len();
+    let g1_wins = solutions_for_game(g1);
+    let g2_wins = solutions_for_game(g2);
+    let g3_wins = solutions_for_game(g3);
+    let g4_wins = solutions_for_game(g4);
+
+    println!("G1 wins: {}", g1_wins);
+    println!("G2 wins: {}", g2_wins);
+    println!("G3 wins: {}", g3_wins);
+    println!("G4 wins: {}", g4_wins);
+
+    let sum = g1_wins * g2_wins * g3_wins * g4_wins;
     println!("Sum: {sum}");
 }
 
-fn solutions_for_game(game: Game) -> Vec<u32> {
-    let mut winnings: Vec<u32> = vec![];
+fn process_part2() {
+    let game = Game {
+        time: 47847467,
+        dist: 207139412091014,
+    };
+    let sum = solutions_for_game(game);
+    println!("Sum: {sum}");
+}
+
+fn solutions_for_game(game: Game) -> u64 {
+    let mut winnings: u64 = 0;
     let half = game.time / 2;
 
     for t in (1..=half).rev() {
         let dist = t * (game.time - t);
         if dist <= game.dist {
-            // println!("T:{} wit dist:{} => out",t, dist);
             break;
         }
-        // println!("T:{} wit dist:{} => in",t, dist);
-        winnings.push(dist);
+        winnings += 1;
     }
-    for t in half+1..game.time {
+    for t in half + 1..game.time {
         let dist = t * (game.time - t);
-        // println!("T:{} wit dist:{} => out",t, dist);
         if dist <= game.dist {
-            // println!("T:{} wit dist:{} => in",t, dist);
             break;
         }
-        winnings.push(dist);
+        winnings += 1;
     }
+
     winnings
 }
